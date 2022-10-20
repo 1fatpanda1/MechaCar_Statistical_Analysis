8451f41e56b0c4802a46db10fc045ed68cc1c96c
Haynn Tang <thanatoscharun@gmail.com>
2022-10-20 14:41:51 -0600

    added deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,4 +5,7 @@ lm(vehicle_length ~ mpg,MechaCar)
 summary(lm(vehicle_length ~ mpg,MechaCar))
 
 
-
+# Deliverable 2
+SC <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- SC %>%  summarize(Mean=mean(PSI),Median=median(PSI),Variance=sd(PSI)^2, SD=sd(PSI), .groups = 'keep')
+lot_summary<- SC %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=sd(PSI)^2, SD=sd(PSI), .groups = 'keep') 
